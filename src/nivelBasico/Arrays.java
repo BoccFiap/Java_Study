b2e0cd91b0803e196b8abd6185c4d20f2d23c0da
bocchi <boccfiap@outlook.com>
2025-06-11 23:39:49 -0300

    Continuing with Arrays and a brief introduction to the Garbage Collector.

--- src/nivelBasico/Arrays.java
+++ src/nivelBasico/Arrays.java
@@ -3,13 +3,31 @@ package nivelBasico;
 public class Arrays {
   public static void main(String[] args) {
 
-    String[] ninja = new String[4];
+    String[] ninja = new String[6];
 
     // String inicializa como -> null
     ninja[0] = "Naruto Uzumaki";
     ninja[1] = "Sasuke Uchiha";
     ninja[2] = "Sakura Haruno";
-    System.out.println(ninja[2]);
+    ninja[3] = "Hinata Hyuga";
+    ninja[4] = "Kakashi hateke";
+    System.out.println(ninja[5]);
+
+    // redeclarar ARRAY
+    ninja = new String[7];
+    ninja[0] = "Hashirama Senju";
+    ninja[1] = "Tobirama Senju";
+    ninja[2] = "Hiruzen Sarutobi ";
+    ninja[3] = "Minato Namikaze";
+    ninja[4] = "Tsunade";
+    ninja[5] = "Kakashi Hatake";
+    ninja[6] = "Naruto Uzumaki";
+
+    // for para fazer um LOOP no array
+    for (int i = 0; i < 7; i++) {
+      System.out.println(ninja[i]);
+    }
+
 
     // Int inicializa como -> 0
     int[] age = new int[2];

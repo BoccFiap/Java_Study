44d684b14d53684c55a1220fdbc04e078fcace70
bocchi <boccfiap@outlook.com>
2025-06-13 15:27:11 -0300

    Learning Inheritance and adding the first inheritance (Uchiha)

---
+++ src/intermediario/Uchiha.java
@@ -0,0 +1,8 @@
+package intermediario;
+
+public class Uchiha extends Ninja{
+
+  public void SharinganActivated() {
+    System.out.println("My name is " +name+ "The Sharingan has been activated, Because I am an Uchiha.");
+  }
+}

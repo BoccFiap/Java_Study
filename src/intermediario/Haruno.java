f58a97449643ce824b306eaef6c81eefe52d59ae
bocchi <boccfiap@outlook.com>
2025-06-15 12:59:59 -0300

    Applying Inheritance to the Ninja class

---
+++ src/intermediario/Haruno.java
@@ -0,0 +1,9 @@
+package intermediario;
+
+public class Haruno extends Ninja {
+
+  public void ActivateHealing(){
+    System.out.println("My name is " +name+ ". And I have activated my extraordinary healing.");
+  }
+
+}
